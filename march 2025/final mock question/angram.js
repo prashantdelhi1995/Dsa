5c97edde839d4ae97abc83d1cb7b6522366ea485
prashant kumar <00udemy00@gmail.com>
2025-03-13 11:29:08 +0530

    final 13_03_2025

---
+++ march 2025/final mock question/angram.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if (s.length !== t.length) {
+         return false;
+     }
+ 
+     let count = {};
+ 
+     for (let char of s) {
+         count[char] = (count[char] || 0) + 1;
+     }
+ 
+     for (let char of t) {
+         if (!count[char]) {
+             return false;
+         }
+         count[char]--;
+     }
+ 
+     return true;
+     
+ };
